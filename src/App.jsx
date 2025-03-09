8723a3d560190553daed062efa2fa74805a8ee45
Marius Ranta <mariusranta@gmail.com>
2025-03-09 12:05:58 +0200

    changes in configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
+import HomepageImage from './components/HomepageImage'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <p>marius . run personal website project</p>
+    <div>
+      <HomepageImage />
+      <h1>marius . run personal website project</h1>
       <p>00010101011101111101101110</p>
-    </>
+    </div>
   )
 }
 
---
+++ src/components/HomepageImage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function HomepageImage() {
+  const url = 'https://wallpapercave.com/wp/8PSLVaF.jpg';
+  return (
+    <img src={url} style={{width: 650}} alt='Sauna' />
+  );
+}
+
+export default HomepageImage;
